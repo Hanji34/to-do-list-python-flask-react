7fba08e79cbd1081bb8f06b945288c45c1d4de95
Hanji34 <victortadashi123@gmail.com>
2020-02-17 19:04:01 -0300

    add routes/endpoints

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,40 @@
-from app import app
+from flask import jsonify, request, abort, url_for
+from app import db, app
+from app.models import Todo
 
-@app.route('/todo')
+@app.route("/todo", methods = ["GET"])
 def list_todo():
-    return "To-do-list"
+    everytodo = []
+    for t in Todo.query.all():
+        everytodo.append(t.to_dict())
+    
+    return jsonify(
+        {
+            "ToDo": everytodo
+        }
+    )
+
+@app.route("/todo", methods = ["POST"])
+def create_todo():
+    requested_todo = request.get_json()
+    todo = Todo(requested_todo["description"], requested_todo["done"])
+    todo.create_todo()
+    print(todo)
+    return jsonify(todo.to_dict())
+
+@app.route("/todo/<int:todoid>", methods = ["GET"])
+def get_todo(todoid):
+    todo = Todo.query.get(todoid)
+    return jsonify(todo.to_dict())
+
+@app.route("/todo/<int:todoid>", methods = ["DELETE"])
+def delete_todo(todoid):
+    todo = Todo.query.get(todoid)
+    todo.delete_todo()
+    return jsonify("To-do deletado com sucesso")
+
+@app.route("/todo/<int:todoid>/finish", methods = ["POST"])
+def update_todo(todoid):
+    todo = Todo.query.get(todoid)
+    todo.update_todo()
+    return jsonify(todo.to_dict())
