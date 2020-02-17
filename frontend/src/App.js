9308138fdbf1f7fedc8f27b55bed5f1786fab02b
Hanji34 <victortadashi123@gmail.com>
2020-02-17 19:05:06 -0300

    add changes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Todo } from './components/Todo';
+import { TodoForm } from './components/TodoForm';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div>
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    axios.get(
+      "http://localhost:5000/todo"
+    ).then(
+      (response) => setTodos(response.data.ToDo)
+    ).catch(
+      (error) => console.log(error)
+    )
+  }, []);
+  const completeTodo = id => {
+    axios.post(
+      `http://localhost:5000/todo/${id}/finish`
+    ).then(
+      (response) => {
+        let newTodos = [...todos]
+        for (let i = 0; i < todos.length; i++) {
+          if (newTodos[i].id === response.data.id) {
+            newTodos[i].done = response.data.done
+          }
+        }
+        setTodos(newTodos)
+      }
+    ).catch((error) => console.log(error))
+  }
 
+  const removeTodo = id => {
+    axios.delete(
+      `http://localhost:5000/todo/${id}`
+    ).then(
+      (_response) => setTodos(todos.filter((t) => t.id !== id))
+    ).catch(
+      (error) => console.log(error))
+  }
+
+  const addTodo = (description) => {
+    axios.post(
+      "http://localhost:5000/todo",
+      { description: description, done: false }
+    ).then(
+      (response) => setTodos([...todos, response.data])
+    ).catch((error) => console.log(error))
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
