679a16549ecfe3c11e4dfade8f22cc285496a8b6
Hanji34 <victortadashi123@gmail.com>
2020-02-17 21:07:44 -0300

    add list change with <select>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,10 @@ import { TodoForm } from './components/TodoForm';
 import axios from 'axios';
 
 
+
 function App() {
+  const [selected, setSelected] = useState("all")
+
   const [todos, setTodos] = useState([])
   useEffect(() => {
     axios.get(
@@ -53,7 +56,7 @@ function App() {
   return (
     <div className="app">
       <div className="todo-list">
-        {todos.map((todo, index) => (
+        {todos.filter((todo) => selected === "all" || (todo.done ? "done" : "undone") === selected).map((todo, index) => (
           <Todo
             key={index}
             index={index}
@@ -62,6 +65,11 @@ function App() {
             removeTodo={removeTodo}
           />
         ))}
+        <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+          <option value="all">All</option>
+          <option value="done">Done</option>
+          <option value="undone">Undone</option>
+        </select>
         <TodoForm addTodo={addTodo} />
       </div>
     </div>
